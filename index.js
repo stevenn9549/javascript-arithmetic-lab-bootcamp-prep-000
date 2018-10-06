224ab62fecfbbc6f635ac31735a318c3205ab5cb
steven real sanchez <stevenn9549@gmail.com>
2018-10-06 05:06:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,10 +19,6 @@ function dec(n){
 }
 function makeInt(n){
   return parseInt(n, 10);
-  }
-  
-  function makeInt(n, base){
-  return parseInt(n, 10);
 }
 function makeInt(n){
   return parseInt('ifvnein');
